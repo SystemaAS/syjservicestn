661887571963ef0cf656ae995d7110dcb7f7c88f
sobekpeleteiro <64634843+sobekpeleteiro@users.noreply.github.com>
2023-06-22 17:01:38 +0200

    Update

--- src/no/systema/jservices/tvinn/sad/ncts5export/model/dao/entities/NctsecDao.java
+++ src/no/systema/jservices/tvinn/sad/ncts5export/model/dao/entities/NctsecDao.java
@@ -20,26 +20,32 @@ public class NctsecDao implements IDao {
 	private Integer tcavd2  = 0; // sonet        4  avdeling FOR HENTET OPD 
 	private Integer tctdn2  = 0; // sonet        7  Tolldeklarasjonsnr FOR HENTET OPD     
 	private String  tcxext  = ""; // tegn       35  Extern.ref.   
-	
+	//AdditionalSupplyChainActor (0-99)
 	private String tcrole  = ""; // tegn        3 Lev.role           
-	private String tcidr  = ""; // tegn        17 Lev.id          
+	private String tcidr  = ""; // tegn        17 Lev.id     
+	//DepartureTransportMeans (0-999)
 	private Integer tctaty = 0; // tegn        2  Type identif.         
 	private String tctaid = ""; // tegn       35  ident         
 	private String tctalk = ""; // sonet      2   landkod - Tr.middel        
+	//Previous Document (0-99)
 	private String tcpdty = ""; // tegn        4  Dok.Type
-	
 	private String tcpdrf   = ""; // tegn      70 Dok.ref 
 	private String tcpdin   = ""; // tegn      35 Info.           
+	//Supporting Document (0-99)
 	private String tcsdty = ""; // tegn        4  Dok.Type
 	private String tcsdrf   = ""; // tegn      70 Dok.ref 
 	private Integer tcsdln   = 0; // sonet      5 Line item nr 
 	private String tcsdin   = ""; // tegn      35 Info.
+	//Transport Document (0-99)
 	private String tctdty = ""; // tegn        4  Dok.Type
-	private String tctdrf   = ""; // tegn      70 Dok.ref 
+	private String tctdrf   = ""; // tegn      70 Dok.ref
+	//Additional Reference (0-99)
 	private String tcadty   = ""; // tegn      4 Ref.type
-	private String tcadrf   = ""; // tegn      70 Dok.ref 
+	private String tcadrf   = ""; // tegn      70 Dok.ref
+	//Additional information (0-99)
 	private String tcaicd   = ""; // tegn      5 info.kod
 	private String tcaitx  = ""; // tegn    120 Text
+	//Transp.Charges (1)
 	private String tctrch  = ""; // tegn    1 Betal.sätt
 
 	
