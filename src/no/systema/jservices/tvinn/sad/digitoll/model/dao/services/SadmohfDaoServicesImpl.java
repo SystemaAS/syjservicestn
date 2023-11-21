2344416f06f6a70a6c80f69df7129030708617e7
sobekpeleteiro <64634843+sobekpeleteiro@users.noreply.github.com>
2023-11-21 15:43:30 +0100

    Update

--- src/no/systema/jservices/tvinn/sad/digitoll/model/dao/services/SadmohfDaoServicesImpl.java
+++ src/no/systema/jservices/tvinn/sad/digitoll/model/dao/services/SadmohfDaoServicesImpl.java
@@ -187,8 +187,12 @@ public class SadmohfDaoServicesImpl implements SadmohfDaoServices {
 			//DEBUG --> logger.info("mydebug");
 			sql.append(" INSERT INTO "  + this.TABLE_NAME +  "( ehavd, ehpro, ehtdn, ehlnrt, ehlnrm, ehlnrh,  ");
 			sql.append(" ehst, ehst2, ehst3, ehuuid, ehmid, ehdts, ehtms, ehrecid, ehcnin, ehvkb, ehntk, ehextref, ");
-			sql.append(" ehvt, ehdkh, ehdkht, ehpr, ehprt, ehupr, ehuprt, ehrg, eh0068a, eh0068b, ehtrnr,  ");
-			sql.append(" ehtrty, ehetyp, ehetypt, eheid, ehkns, ehrgs, ehtpps, ehnas, ehna2s, ehad1s, ehnrs,  ");
+			sql.append(" ehvt, ehdkh, ehdkht, ehpr, ehprt, ehupr, ehuprt, ehrg, eh0068a, eh0068b, ehtrnr, ehtrty, ");
+			//
+			sql.append(" ehtrnr2, ehtrty2, ehtrnr3, ehtrty3, ehtrnr4, ehtrty4, ehtrnr5, ehtrty5,  ehtrnr6, ehtrty6,  ");
+			sql.append(" ehtrnr7, ehtrty7, ehtrnr8, ehtrty8, ehtrnr9, ehtrty9, ehtrnr10, ehtrty10,   ");
+			//
+			sql.append(" ehetyp, ehetypt, eheid, ehkns, ehrgs, ehtpps, ehnas, ehna2s, ehad1s, ehnrs,  ");
 			//
 			sql.append(" ehetyp2, ehetypt2, eheid2, ehetyp3, ehetypt3, eheid3, ehetyp4, ehetypt4, eheid4, ehetyp5, ehetypt5, eheid5,   ");
 			sql.append(" ehetyp6, ehetypt6, eheid6, ehetyp7, ehetypt7, eheid7, ehetyp8, ehetypt8, eheid8, ehetyp9, ehetypt9, eheid9, ehetyp10, ehetypt10, eheid10,   ");
@@ -198,9 +202,13 @@ public class SadmohfDaoServicesImpl implements SadmohfDaoServices {
 			
 			sql.append(" VALUES ( ?,?,?,?,?,?, ");
 			sql.append(" ?,?,?,?,?,?,?,?,?,?,?,?, ");
-			sql.append(" ?,?,?,?,?,?,?,?,?,?,?, ");
-			sql.append(" ?,?,?,?,?,?,?,?,?,?,?, ");
+			sql.append(" ?,?,?,?,?,?,?,?,?,?,?,?, ");
+			//flera transiteringar
+			sql.append(" ?,?,?,?,?,?,?,?,?,?, ");
+			sql.append(" ?,?,?,?,?,?,?,?, ");
 			//
+			sql.append(" ?,?,?,?,?,?,?,?,?,?, ");
+			//flera exporter
 			sql.append(" ?,?,?,?,?,?,?,?,?,?,?,?,  ");
 			sql.append(" ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?, ");
 			//
@@ -211,10 +219,16 @@ public class SadmohfDaoServicesImpl implements SadmohfDaoServices {
 			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { 
 			dao.getEhavd(), dao.getEhpro(), dao.getEhtdn(), dao.getEhlnrt(), dao.getEhlnrm(), dao.getEhlnrh(),		
 			dao.getEhst(), dao.getEhst2(), dao.getEhst3(), dao.getEhuuid(), dao.getEhmid(), dao.getEhdts(), dao.getEhtms(), dao.getEhrecid(), dao.getEhcnin(), dao.getEhvkb(), dao.getEhntk(), dao.getEhextref(),
-			dao.getEhvt(), dao.getEhdkh(), dao.getEhdkht(), dao.getEhpr(), dao.getEhprt(), dao.getEhupr(), dao.getEhuprt(), dao.getEhrg(), dao.getEh0068a(), dao.getEh0068b(), dao.getEhtrnr(),
-			dao.getEhtrty(), dao.getEhetyp(), dao.getEhetypt(), dao.getEheid(), dao.getEhkns(), dao.getEhrgs(), dao.getEhtpps(), dao.getEhnas(), dao.getEhna2s(), dao.getEhad1s(), dao.getEhnrs(),
+			dao.getEhvt(), dao.getEhdkh(), dao.getEhdkht(), dao.getEhpr(), dao.getEhprt(), dao.getEhupr(), dao.getEhuprt(), dao.getEhrg(), dao.getEh0068a(), dao.getEh0068b(), dao.getEhtrnr(),dao.getEhtrty(), 
+			//flera transiteringar
+			dao.getEhtrnr2(),dao.getEhtrty2(), dao.getEhtrnr3(),dao.getEhtrty3(), dao.getEhtrnr4(),dao.getEhtrty4(), dao.getEhtrnr5(),dao.getEhtrty5(), dao.getEhtrnr6(),dao.getEhtrty6(), 
+			dao.getEhtrnr7(),dao.getEhtrty7(), dao.getEhtrnr8(),dao.getEhtrty8(), dao.getEhtrnr9(),dao.getEhtrty9(), dao.getEhtrnr10(),dao.getEhtrty10(), 
+			//
+			dao.getEhetyp(), dao.getEhetypt(), dao.getEheid(), dao.getEhkns(), dao.getEhrgs(), dao.getEhtpps(), dao.getEhnas(), dao.getEhna2s(), dao.getEhad1s(), dao.getEhnrs(),
+			//flera exporter
 			dao.getEhetyp2(), dao.getEhetypt2(), dao.getEheid2(), dao.getEhetyp3(), dao.getEhetypt3(), dao.getEheid3(), dao.getEhetyp4(), dao.getEhetypt4(), dao.getEheid4(), dao.getEhetyp5(), dao.getEhetypt5(), dao.getEheid5(),
 			dao.getEhetyp6(), dao.getEhetypt6(), dao.getEheid6(), dao.getEhetyp7(), dao.getEhetypt7(), dao.getEheid7(), dao.getEhetyp8(), dao.getEhetypt8(), dao.getEheid8(), dao.getEhetyp9(), dao.getEhetypt9(), dao.getEheid9(), dao.getEhetyp10(), dao.getEhetypt10(), dao.getEheid10(),
+			//
 			dao.getEhpns(), dao.getEhpss(), dao.getEhlks(), dao.getEhpbs(), dao.getEhems(), dao.getEhemst(), dao.getEhknm(), dao.getEhrgm(), dao.getEhtppm(), dao.getEhnam(), dao.getEhna2m(), dao.getEhad1m(), dao.getEhnrm(), dao.getEhpnm(),
 			dao.getEhpsm(), dao.getEhlkm(), dao.getEhpbm(), dao.getEhemm(), dao.getEhemmt(), dao.getEhlka(), dao.getEhsda(), dao.getEhsdat(), dao.getEhlkd(), dao.getEhsdd(),  dao.getEhsddt(),
 			
@@ -249,8 +263,12 @@ public class SadmohfDaoServicesImpl implements SadmohfDaoServices {
 			//DEBUG --> logger.info("mydebug");
 			sql.append(" UPDATE "  + this.TABLE_NAME +  " SET  ehavd = ?, ehpro = ?, ehtdn = ?,   ");
 			sql.append(" ehst = ?, ehst2 = ?, ehst3 = ?, ehuuid = ?, ehmid = ?, ehdts = ?, ehtms = ?, ehrecid = ?, ehcnin = ?, ehvkb = ?, ehntk = ?, ehextref = ?, ");
-			sql.append(" ehvt = ?, ehdkh = ?, ehdkht = ?, ehpr = ?, ehprt = ?, ehupr = ?, ehuprt = ?, ehrg = ?, eh0068a = ?, eh0068b = ?, ehtrnr = ?,  ");
-			sql.append(" ehtrty = ?, ehetyp = ?, ehetypt = ?, eheid = ?, ehkns = ?, ehrgs = ?, ehtpps = ?, ehnas = ?, ehna2s = ?, ehad1s = ?, ehnrs = ?,  ");
+			sql.append(" ehvt = ?, ehdkh = ?, ehdkht = ?, ehpr = ?, ehprt = ?, ehupr = ?, ehuprt = ?, ehrg = ?, eh0068a = ?, eh0068b = ?, ehtrnr = ?, ehtrty = ?,  ");
+			//
+			sql.append(" ehtrnr2 = ?, ehtrty2 = ?, ehtrnr3 = ?, ehtrty3 = ?, ehtrnr4 = ?, ehtrty4 = ?, ehtrnr5 = ?, ehtrty5 = ?, ehtrnr6 = ?, ehtrty6 = ?, "); 
+			sql.append(" ehtrnr7 = ?, ehtrty7 = ?, ehtrnr8 = ?, ehtrty8 = ?, ehtrnr9 = ?, ehtrty9 = ?, ehtrnr10 = ?, ehtrty10 = ?,  "); 
+			//
+			sql.append(" ehetyp = ?, ehetypt = ?, eheid = ?, ehkns = ?, ehrgs = ?, ehtpps = ?, ehnas = ?, ehna2s = ?, ehad1s = ?, ehnrs = ?,  ");
 			//
 			sql.append(" ehetyp2 = ?, ehetypt2 = ?, eheid2 = ?, ehetyp3 = ?, ehetypt3 = ?, eheid3 = ?, ehetyp4 = ?, ehetypt4 = ?, eheid4 = ?, ehetyp5 = ?, ehetypt5 = ?, eheid5 = ?,   ");
 			sql.append(" ehetyp6 = ?, ehetypt6 = ?, eheid6 = ?, ehetyp7 = ?, ehetypt7 = ?, eheid7 = ?, ehetyp8 = ?, ehetypt8 = ?, eheid8 = ?, ehetyp9 = ?, ehetypt9 = ?, eheid9 = ?, ehetyp10 = ?, ehetypt10 = ?, eheid10 = ?,   ");
@@ -264,8 +282,12 @@ public class SadmohfDaoServicesImpl implements SadmohfDaoServices {
 			retval = this.jdbcTemplate.update( sql.toString(), new Object[] { 
 			dao.getEhavd(), dao.getEhpro(), dao.getEhtdn(), 		
 			dao.getEhst(), dao.getEhst2(), dao.getEhst3(), dao.getEhuuid(), dao.getEhmid(), dao.getEhdts(), dao.getEhtms(), dao.getEhrecid(), dao.getEhcnin(), dao.getEhvkb(), dao.getEhntk(), dao.getEhextref(),
-			dao.getEhvt(), dao.getEhdkh(), dao.getEhdkht(), dao.getEhpr(), dao.getEhprt(), dao.getEhupr(), dao.getEhuprt(), dao.getEhrg(), dao.getEh0068a(), dao.getEh0068b(), dao.getEhtrnr(),
-			dao.getEhtrty(), dao.getEhetyp(), dao.getEhetypt(), dao.getEheid(), dao.getEhkns(), dao.getEhrgs(), dao.getEhtpps(), dao.getEhnas(), dao.getEhna2s(), dao.getEhad1s(), dao.getEhnrs(),
+			dao.getEhvt(), dao.getEhdkh(), dao.getEhdkht(), dao.getEhpr(), dao.getEhprt(), dao.getEhupr(), dao.getEhuprt(), dao.getEhrg(), dao.getEh0068a(), dao.getEh0068b(), dao.getEhtrnr(),dao.getEhtrty(),
+			//
+			dao.getEhtrnr2(),dao.getEhtrty2(), dao.getEhtrnr3(),dao.getEhtrty3(), dao.getEhtrnr4(),dao.getEhtrty4(), dao.getEhtrnr5(),dao.getEhtrty5(), dao.getEhtrnr6(),dao.getEhtrty6(),
+			dao.getEhtrnr7(),dao.getEhtrty7(), dao.getEhtrnr8(),dao.getEhtrty8(), dao.getEhtrnr9(),dao.getEhtrty9(), dao.getEhtrnr10(),dao.getEhtrty10(),
+			//
+			dao.getEhetyp(), dao.getEhetypt(), dao.getEheid(), dao.getEhkns(), dao.getEhrgs(), dao.getEhtpps(), dao.getEhnas(), dao.getEhna2s(), dao.getEhad1s(), dao.getEhnrs(),
 			//
 			dao.getEhetyp2(), dao.getEhetypt2(), dao.getEheid2(), dao.getEhetyp3(), dao.getEhetypt3(), dao.getEheid3(), dao.getEhetyp4(), dao.getEhetypt4(), dao.getEheid4(), dao.getEhetyp5(), dao.getEhetypt5(), dao.getEheid5(),
 			dao.getEhetyp6(), dao.getEhetypt6(), dao.getEheid6(), dao.getEhetyp7(), dao.getEhetypt7(), dao.getEheid7(), dao.getEhetyp8(), dao.getEhetypt8(), dao.getEheid8(), dao.getEhetyp9(), dao.getEhetypt9(), dao.getEheid9(), dao.getEhetyp10(), dao.getEhetypt10(), dao.getEheid10(),
